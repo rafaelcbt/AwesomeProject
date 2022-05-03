333d99ca9d0c01c2d2e996f5d9b5cd5c68860e3b
rafaelcbt <rafaelcamararag@hotmail.com>
2022-05-02 21:02:40 -0300

    App.js modification

--- App.js
+++ App.js
@@ -7,16 +7,20 @@ export default function App(){
 
 
   //Função pegaNome
-   const [nome,setNome]=useState('Testando Nome...');
-   const [idade,setIdade]=useState('Testando Idade...');
+   const [nome,setNome]=useState('Verificando Nome...');
+   const [idade,setIdade]=useState('Verificando Idade...');
+   const [curso,setCurso]=useState('Verificando Curso...');
+   const [modulo,setModulo]=useState('Verificando Módulo...');
   
   
     useEffect(()=>{
      
       async function Dados(){
-          await firebase.database().ref('usuarios/1').on('value',(snapshot)=>{
+          await firebase.database().ref('usuarios/3').on('value',(snapshot)=>{
               setNome(snapshot.val().nome);
               setIdade(snapshot.val().idade);
+              setCurso(snapshot.val().curso);
+              setModulo(snapshot.val().modulo);
   
           })
       }
@@ -25,8 +29,10 @@ export default function App(){
   
       return(
           <View style ={{marginTop:25}}>
-              <Text>{nome}</Text>
-              <Text>{idade}</Text>
+              <Text>Aluno: {nome}</Text>
+              <Text>Idade: {idade}</Text>
+              <Text>Curso: {curso}</Text>
+              <Text>Módulo: {modulo}</Text>
           </View>
       );
   }
